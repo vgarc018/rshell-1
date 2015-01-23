b45bdd9035b2742814e174663cc0ed737a0320d9
Selik Antonvinod <santo003@hammer.cs.ucr.edu>
2015-01-23 15:48:02 -0800

    added exit and some tokenizing

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -4,40 +4,47 @@
 #include <errno.h>
 #include <boost/tokenizer.hpp>
 #include <string>
+#include <vector>
+#include <queue>
 
 using namespace std;
 using namespace boost;
 
-int main(int argc, char **argv){ //main can take cmd line args
-
-
-
+bool exit_flag = false;
+string exiting = "exit";
 string str = "";		//used to create a new substring for each word
-string cmd;
-std::getline(std::cin, cmd);
-typedef boost::tokenizer <boost::char_separator<char> > Tok;
-boost::char_separator<char> sep;
-Tok tok(cmd, sep);
-	for(Tok::iterator beg=tok.begin(); beg!=tok.end();++beg){
-		cout << *beg << endl;
 
-		str.append(*beg);
-	}
-/*
-int pid = fork();	 //creating a process id/child process
-
-	if(pid == -1){
-		perror("fork failed");
-	}
-	else if(pid == 0){	//in the child process if pid = 0
-		//do task
-	}
-	else if(pid > 0) {
-		//now in parent 
+int num_of_args = 0;
+queue <string> commands;
+void andcmd(const char* s) {
+	
+}
+queue <string> line;
+int i = 0;		//used as index of argv in main
+int main(int argc, char **argv){ //main can take cmd line args
+string nocomment = "";
+	while(exit_flag == false){
+			
+		cout << "$: ";
+		string cmd;	
+		std::getline(std::cin, cmd);
 		
-	}
-	*/
-	cout << str << endl;
-//	std::cout << cmd <<std::endl;
+		str = "";
+
+		typedef boost::tokenizer <boost::char_separator<char> > Tok;
+		boost::char_separator<char> sep;
+		Tok tok(cmd, sep);
+		for(Tok::iterator beg=tok.begin(); beg!=tok.end();++beg){
+			cout << *beg << endl;
+			str.append(*beg);
+		
+		}
+			if(str.compare("exit") == 0) { exit_flag = true;}	
+			str = "";
+			
+			
+		}
+			
+//	cout <<&str << endl;
 	return 0;
 }
